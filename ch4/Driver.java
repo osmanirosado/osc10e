4d8fb735e148f08ac49099fb7e30922cd1a1c59b
Osmani Rosado-Falcon <osmanir@uclv.cu>
2021-12-28 11:10:05 -0500

    Summation program modification. Set a constant value for the upper variable to simplify the execution of this example. Resolve some IDE warnings.

--- ch4/Driver.java
+++ ch4/Driver.java
@@ -1,18 +1,17 @@
 /**
  * Summation program using exectuors/callable/futures
- *
+ * <p>
  * Figure 4.14
  *
  * @author Gagne, Galvin, Silberschatz
  * Operating System Concepts  - Tenth Edition
  * Copyright John Wiley & Sons - 2018
  */
- 
+
 import java.util.concurrent.*;
 
-class Summation implements Callable<Integer>
-{
-    private int upper;
+class Summation implements Callable<Integer> {
+    private final int upper;
 
     public Summation(int upper) {
         this.upper = upper;
@@ -22,27 +21,23 @@ class Summation implements Callable<Integer>
         int sum = 0;
         for (int i = 1; i <= upper; i++)
             sum += i;
-    
-        return new Integer(sum);
+        return sum;
     }
 }
 
 
-public class Driver
-{
+public class Driver {
     public static void main(String[] args) {
-        if (args.length == 1) {
-            int upper = Integer.parseInt(args[0]);
+        int upper = 256;
 
-            ExecutorService pool = Executors.newSingleThreadExecutor();
-            Future<Integer> result = pool.submit(new Summation(upper));
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        Future<Integer> result = pool.submit(new Summation(upper));
 
-            try {
-                System.out.println("sum = " + result.get());
-            }
-            catch (InterruptedException | ExecutionException ie) { }
-
-            pool.shutdown();
+        try {
+            System.out.println("sum = " + result.get());
+        } catch (InterruptedException | ExecutionException ie) {
         }
+
+        pool.shutdown();
     }
 }
