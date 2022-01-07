25832e065b0fe43431e0818745e5e3108318f61d
Osmani Rosado-Falcon <osmanir@uclv.cu>
2022-01-07 12:04:38 -0500

    Reformat code of the SumTask classes and moving one attribute declaration sentence from one line to another.

--- ch4/SumTask.java
+++ ch4/SumTask.java
@@ -1,6 +1,6 @@
 /**
  * Fork/join parallelism in Java
- *
+ * <p>
  * Figure 4.18
  *
  * @author Gagne, Galvin, Silberschatz
@@ -12,9 +12,7 @@ import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class SumTask extends RecursiveTask<Integer>
-{
-    static final int SIZE = 10000;
+public class SumTask extends RecursiveTask<Integer> {
     static final int THRESHOLD = 1000;
 
     private final int begin;
@@ -35,11 +33,10 @@ public class SumTask extends RecursiveTask<Integer>
                 sum += array[i];
 
             return sum;
-        }
-        else {
+        } else {
             // divide stage 
             int mid = begin + (end - begin) / 2;
-            
+
             SumTask leftTask = new SumTask(begin, mid, array);
             SumTask rightTask = new SumTask(mid + 1, end, array);
 
@@ -50,19 +47,21 @@ public class SumTask extends RecursiveTask<Integer>
         }
     }
 
-	public static void main(String[] args) {
-		ForkJoinPool pool = new ForkJoinPool();
+    static final int SIZE = 10000;
 
-		// create SIZE random integers between 0 and 9
+    public static void main(String[] args) {
+        ForkJoinPool pool = new ForkJoinPool();
+
+        // create SIZE random integers between 0 and 9
         Random random = new Random();
         int[] array = random.ints(0, 9).limit(SIZE).toArray();
-		
-		// use fork-join parallelism to sum the array
-		SumTask task = new SumTask(0, SIZE-1, array);
 
-		int sum = pool.invoke(task);
+        // use fork-join parallelism to sum the array
+        SumTask task = new SumTask(0, SIZE - 1, array);
 
-		System.out.println("The sum is " + sum);
-	}
+        int sum = pool.invoke(task);
+
+        System.out.println("The sum is " + sum);
+    }
 }
 
--- ch4/SumTask2.java
+++ ch4/SumTask2.java
@@ -1,6 +1,6 @@
 /**
  * Fork/join parallelism in Java
- *
+ * <p>
  * Figure 4.18
  *
  * @author Gagne, Galvin, Silberschatz
@@ -12,9 +12,7 @@ import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class SumTask2 extends RecursiveTask<Integer>
-{
-    static final int SIZE = 10000;
+public class SumTask2 extends RecursiveTask<Integer> {
     static final int THRESHOLD = 1000;
 
     private final int begin;
@@ -35,11 +33,10 @@ public class SumTask2 extends RecursiveTask<Integer>
                 sum += array[i];
 
             return sum;
-        }
-        else {
+        } else {
             // divide stage 
             int mid = begin + (end - begin) / 2;
-            
+
             SumTask2 leftTask = new SumTask2(begin, mid, array);
             SumTask2 rightTask = new SumTask2(mid + 1, end, array);
 
@@ -49,19 +46,21 @@ public class SumTask2 extends RecursiveTask<Integer>
         }
     }
 
-	public static void main(String[] args) {
-		ForkJoinPool pool = new ForkJoinPool();
+    static final int SIZE = 10000;
 
-		// create SIZE random integers between 0 and 9
+    public static void main(String[] args) {
+        ForkJoinPool pool = new ForkJoinPool();
+
+        // create SIZE random integers between 0 and 9
         Random random = new Random();
         int[] array = random.ints(0, 9).limit(SIZE).toArray();
-		
-		// use fork-join parallelism to sum the array
-		SumTask2 task = new SumTask2(0, SIZE-1, array);
 
-		int sum = pool.invoke(task);
+        // use fork-join parallelism to sum the array
+        SumTask2 task = new SumTask2(0, SIZE - 1, array);
 
-		System.out.println("The sum is " + sum);
-	}
+        int sum = pool.invoke(task);
+
+        System.out.println("The sum is " + sum);
+    }
 }
 
